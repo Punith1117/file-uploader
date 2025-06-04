b8fe57662e8292b9f6d30ad5ee1562724fb3f93f
Punith1117 <punith11172006@gmail.com>
2025-06-04 23:51:31 +0530

    add rendering of ejs when unauthenticated

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 function isAuthenticated(req, res, next) {
     if (req.isAuthenticated())
         return next()
-    res.status(401).send('Unauthorized')
+    res.status(401).render('unauthenticated')
 }
 
 module.exports = { isAuthenticated }
